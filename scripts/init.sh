e2d597e078fd960c4bdc9c5486130de79da18bed
mario <mario@maniox.de>
2016-02-28 11:13:10 +0100

    Ref T77 fix base image name, add initial composer.json to source dir

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+#load settings
+source "$DOC_SETTINGS"
+
 DOC_TYPO3_VERSION=${2:-7.6}
 DOC_DOCKERFILE_TYPO3="./dockerfiles/Dockerfile-typo3"
 
-docker inspect "$DOC_USERNAME"/typo3:"$DOC_TYPO3_VERSION" &> /dev/null
+docker inspect "$DOC_USERNAME"/typo3base:"$DOC_TYPO3_VERSION" &> /dev/null
 
 if [ $? -ne 0 ]; then
-	echo "start building TYPO3 $DOC_TYPO3_VERSION"
+	echo "-> start building TYPO3 $DOC_TYPO3_VERSION"
 	docker build \
 	    --build-arg user_id=$(id -u) \
 	    --build-arg group_id=$(id -g) \
 	    --build-arg typo3_version=$DOC_TYPO3_VERSION \
 	    -f "$DOC_DOCKERFILE_TYPO3" \
-	    -t "$DOC_USERNAME"/typo3base:"$DOC_TYPO3_VERSION" .
+	    -t "$DOC_REPO/$DOC_USERNAME/typo3base:$DOC_TYPO3_VERSION" .
 fi
 
+echo "-> copy composer.json from config/composer.json"
+cp config/composer.json sources/composer.json
