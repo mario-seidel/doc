c15502ad027f09951252a413d6ea9cb7125843d3
mario <mario@maniox.de>
2016-02-28 11:10:32 +0100

    Ref T77 start composer at runtime if lock file is missing

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,10 +5,10 @@ echo "EntryPoint"
 #RUN chown -R www-data:www-data /var/www/html && \
 
 #TODO: need 2 ways here?
-if [ -e version.txt ]; then
+if [ -e composer.lock ]; then
 	apache2-foreground
 else
-	#composer.phar update --prefer-stable --prefer-source && \
-	#	echo "0.1" > version.txt && \
-	apache2-foreground
+	composer.phar update --prefer-stable --prefer-source && \
+	    touch FIRST_INSTALL && \
+	    apache2-foreground
 fi
