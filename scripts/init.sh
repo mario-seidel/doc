bc6ecf11e0e97ab32bbb9987297e66e375857acf
Mario Seidel <mario.seidel@dmk-ebusiness.de>
2016-11-22 14:45:08 +0100

    add vhost config, docker logs command, change to ### marker

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,7 @@
 source "$DOC_SETTINGS"
 
 DOC_TYPO3_VERSION=${2:-7.6}
+GIT_REPO=$1
 DOC_DOCKERFILE_TYPO3="./dockerfiles/Dockerfile-typo3"
 
 docker inspect "$DOC_USERNAME"/typo3base:"$DOC_TYPO3_VERSION" &> /dev/null
@@ -11,12 +12,20 @@ docker inspect "$DOC_USERNAME"/typo3base:"$DOC_TYPO3_VERSION" &> /dev/null
 if [ $? -ne 0 ]; then
 	echo "-> start building TYPO3 $DOC_TYPO3_VERSION"
 	docker build \
-	    --build-arg user_id=$(id -u) \
-	    --build-arg group_id=$(id -g) \
-	    --build-arg typo3_version=$DOC_TYPO3_VERSION \
+	    --build-arg hostuser=${HOST_USER} \
+	    --build-arg hostuserid=${HOST_USERID} \
+	    --build-arg hostgroup=${HOST_GROUP} \
+	    --build-arg hostgroupid=${HOST_GROUPID} \
+	    --build-arg typo3_version=${DOC_TYPO3_VERSION} \
 	    -f "$DOC_DOCKERFILE_TYPO3" \
-	    -t "$DOC_REPO/$DOC_USERNAME/typo3base:$DOC_TYPO3_VERSION" .
+	    -t "$DOC_USERNAME/typo3base:$DOC_TYPO3_VERSION" .
 fi
 
-echo "-> copy composer.json from config/composer.json"
-cp config/composer.json sources/composer.json
+if [ -z ${GIT_REPO} ]; then
+	echo "-> copy composer.json from config/composer.json"
+	cp config/composer.json sources/composer.json
+else
+	echo "-> init project from $GIT_REPO"
+	rm -rf ./sources
+	git checkout ${GIT_REPO} sources
+fi
